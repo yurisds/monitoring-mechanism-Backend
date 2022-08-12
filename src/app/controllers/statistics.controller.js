12e9956b831f5e8bae7b35e3ebe65de178d64be2
Yuri Souza <yuri.santos@ccc.ufcg.edu.br>
2022-08-11 23:09:47 -0300

    Download csv

--- src/app/controllers/statistics.controller.js
+++ src/app/controllers/statistics.controller.js
@@ -2,6 +2,9 @@ const statisticsService = require("../services/statistics.service");
 const eventsService = require("../services/events.service");
 const dayjs = require("dayjs");
 
+const path = require('path');
+const csvWriter = require('csv-writer');
+
 const util = require("../services/util.service");
 
 const getAllStatistics = async (req, res) => {
@@ -18,6 +21,47 @@ const getAllStatistics = async (req, res) => {
         const result = await eventsService.getAllEventsLogs(initialDate, finalDate, event_name);
 
         const extract = await statisticsService.getAllStatistics(result);
+
+        return res.status(200).json(extract);
+
+    } catch (error) {
+        return res.status(500).json({ error: `Ocorreu um erro: ${error.message}` });  
+    }
+
+}
+
+const generateAllStatisticsCSV = async (req, res) => {
+
+    try {
+        const { initialDate, finalDate, event_name } = req.query;
+
+        if (initialDate && finalDate) {
+            if(!dayjs(initialDate).isValid() || !dayjs(finalDate).isValid()){
+                return res.status(400).json({ "message": "Incorrect dates" }); 
+            }
+        }
+
+        const result = await eventsService.getAllEventsLogs(initialDate, finalDate, event_name);
+
+        const extract = await statisticsService.getAllStatistics(result);
+
+        let header = []
+
+        if(extract.length > 0) {
+            Object.keys(extract[0]).forEach(element => {
+                header.push( {id: element, title: element })
+            });
+        }
+
+
+        const writer = csvWriter.createObjectCsvWriter({
+            path: path.resolve(__dirname, 'db_comands.csv'),
+            header: header
+          });
+
+        const writeFilePromisificado = await writer.writeRecords(extract);
+        
+        await writeFilePromisificado
 
         return res.status(200).json(extract);
 
@@ -60,5 +104,6 @@ const getStatisticsByDatabase = async (req, res) => {
 
 module.exports = {
     getAllStatistics,
-    getStatisticsByDatabase
+    getStatisticsByDatabase,
+    generateAllStatisticsCSV
 }
--- src/app/routes/statistics.route.js
+++ src/app/routes/statistics.route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const resultQueryController = require('../controllers/statistics.controller');
 
 router.get('/', resultQueryController.getAllStatistics);
+router.get('/csv', resultQueryController.generateAllStatisticsCSV);
 router.get('/:databaseName', resultQueryController.getStatisticsByDatabase);
 
 module.exports = router;
